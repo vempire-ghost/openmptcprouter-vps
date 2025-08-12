9d1798fe39cf10fcee98999ba37a8fe6118f8522
vempire-ghost <132581535+vempire-ghost@users.noreply.github.com>
2025-08-12 00:40:38 -0300

    Update debian13-x86_64.sh

--- debian13-x86_64.sh
+++ debian13-x86_64.sh
@@ -416,46 +416,65 @@ if [ -z "$(dpkg-query -l | grep grub)" ]; then
 	}
 fi
 if [ "$KERNEL" = "5.4" ] || [ "$KERNEL" = "5.15" ]; then
-	if [ "$SOURCES" = "yes" ]; then
-		wget -O /tmp/linux-image-${KERNEL_RELEASE}_amd64.deb ${VPSURL}kernel/linux-image-${KERNEL_RELEASE}_amd64.deb
-		wget -O /tmp/linux-headers-${KERNEL_RELEASE}_amd64.deb ${VPSURL}kernel/linux-headers-${KERNEL_RELEASE}_amd64.deb
-		# Rename bzImage to vmlinuz, needed when custom kernel was used
-		cd /boot
-		apt-get -y install git
-		rename 's/^bzImage/vmlinuz/s' * >/dev/null 2>&1
-		#apt-get -y install linux-mptcp
-		#dpkg --remove --force-remove-reinstreq linux-image-${KERNEL_VERSION}-mptcp
-		#dpkg --remove --force-remove-reinstreq linux-headers-${KERNEL_VERSION}-mptcp
-		if [ "$(dpkg -l | grep linux-image-${KERNEL_VERSION} | grep ${KERNEL_PACKAGE_VERSION})" = "" ]; then
-			echo "Install kernel linux-image-${KERNEL_RELEASE} source release"
-			echo "\033[1m !!! if kernel install fail run: dpkg --remove --force-remove-reinstreq linux-image-${KERNEL_VERSION}-mptcp !!! \033[0m"
-			dpkg --force-all -i -B /tmp/linux-headers-${KERNEL_RELEASE}_amd64.deb
-			dpkg --force-all -i -B /tmp/linux-image-${KERNEL_RELEASE}_amd64.deb
-		fi
-	else
-		cd /boot
-		rename 's/^bzImage/vmlinuz/s' * >/dev/null 2>&1
-		if [ "$(dpkg -l | grep linux-image-${KERNEL_VERSION} | grep ${KERNEL_PACKAGE_VERSION})" = "" ]; then
-			echo "Install kernel linux-image-${KERNEL_RELEASE}"
-			echo "\033[1m !!! if kernel install fail run: dpkg --remove --force-remove-reinstreq linux-image-${KERNEL_VERSION}-mptcp !!! \033[0m"
-			apt-get -y install linux-image-${KERNEL_VERSION}-mptcp=${KERNEL_PACKAGE_VERSION} linux-headers-${KERNEL_VERSION}-mptcp=${KERNEL_PACKAGE_VERSION}
-		fi
-	fi
+    if [ "$SOURCES" = "yes" ]; then
+        wget -O /tmp/linux-image-${KERNEL_RELEASE}_amd64.deb ${VPSURL}kernel/linux-image-${KERNEL_RELEASE}_amd64.deb
+        wget -O /tmp/linux-headers-${KERNEL_RELEASE}_amd64.deb ${VPSURL}kernel/linux-headers-${KERNEL_RELEASE}_amd64.deb
+        # Rename bzImage to vmlinuz, needed when custom kernel was used
+        cd /boot
+        apt-get -y install git
+        rename 's/^bzImage/vmlinuz/s' * >/dev/null 2>&1
+        if [ "$(dpkg -l | grep linux-image-${KERNEL_VERSION} | grep ${KERNEL_PACKAGE_VERSION})" = "" ]; then
+            echo "Install kernel linux-image-${KERNEL_RELEASE} source release"
+            echo "\033[1m !!! if kernel install fail run: dpkg --remove --force-remove-reinstreq linux-image-${KERNEL_VERSION}-mptcp !!! \033[0m"
+            dpkg --force-all -i -B /tmp/linux-headers-${KERNEL_RELEASE}_amd64.deb
+            dpkg --force-all -i -B /tmp/linux-image-${KERNEL_RELEASE}_amd64.deb
+        fi
+    else
+        cd /boot
+        rename 's/^bzImage/vmlinuz/s' * >/dev/null 2>&1
+        if [ "$(dpkg -l | grep linux-image-${KERNEL_VERSION} | grep ${KERNEL_PACKAGE_VERSION})" = "" ]; then
+            echo "Install kernel linux-image-${KERNEL_RELEASE}"
+            echo "\033[1m !!! if kernel install fail run: dpkg --remove --force-remove-reinstreq linux-image-${KERNEL_VERSION}-mptcp !!! \033[0m"
+            apt-get -y install linux-image-${KERNEL_VERSION}-mptcp=${KERNEL_PACKAGE_VERSION} linux-headers-${KERNEL_VERSION}-mptcp=${KERNEL_PACKAGE_VERSION}
+        fi
+    fi
 
+    # Adicionar módulos essenciais ao initramfs para VMware
+    echo "Adding essential modules to initramfs..."
+    if [ ! -f /etc/initramfs-tools/modules ]; then
+        touch /etc/initramfs-tools/modules
+    fi
+    
+    # Verifica se os módulos já existem no arquivo
+    if ! grep -q "mptspi" /etc/initramfs-tools/modules; then
+        echo "mptspi" >> /etc/initramfs-tools/modules
+    fi
+    
+    if ! grep -q "sd_mod" /etc/initramfs-tools/modules; then
+        echo "sd_mod" >> /etc/initramfs-tools/modules
+    fi
+    
+    if ! grep -q "ext4" /etc/initramfs-tools/modules; then
+        echo "ext4" >> /etc/initramfs-tools/modules
+    fi
 
-	# Check if mptcp kernel is grub default kernel
-	echo "Set MPTCP kernel as grub default..."
-	if [ "$LOCALFILES" = "no" ]; then
-		wget -O /tmp/update-grub.sh ${VPSURL}${VPSPATH}/update-grub.sh
-		cd /tmp
-	else
-		cd ${DIR}
-	fi
-	[ -f /boot/grub/grub.cfg ] && [ -z "$(grep ${KERNEL_VERSION}-mptcp /boot/grub/grub.cfg)" ] && [ -n "$(which grub-mkconfig)" ] && grub-mkconfig -o /boot/grub/grub.cfg
-	rm -f /etc/grub.d/30_os-prober
-	bash update-grub.sh ${KERNEL_VERSION}-mptcp
-	bash update-grub.sh ${KERNEL_RELEASE}
-	[ -f /boot/grub/grub.cfg ] && sed -i 's/default="1>0"/default="0"/' /boot/grub/grub.cfg >/dev/null 2>&1
+    # Atualizar initramfs para o kernel instalado
+    echo "Updating initramfs for kernel ${KERNEL_RELEASE}..."
+    update-initramfs -u -k ${KERNEL_RELEASE}
+
+    # Check if mptcp kernel is grub default kernel
+    echo "Set MPTCP kernel as grub default..."
+    if [ "$LOCALFILES" = "no" ]; then
+        wget -O /tmp/update-grub.sh ${VPSURL}${VPSPATH}/update-grub.sh
+        cd /tmp
+    else
+        cd ${DIR}
+    fi
+    [ -f /boot/grub/grub.cfg ] && [ -z "$(grep ${KERNEL_VERSION}-mptcp /boot/grub/grub.cfg)" ] && [ -n "$(which grub-mkconfig)" ] && grub-mkconfig -o /boot/grub/grub.cfg
+    rm -f /etc/grub.d/30_os-prober
+    bash update-grub.sh ${KERNEL_VERSION}-mptcp
+    bash update-grub.sh ${KERNEL_RELEASE}
+    [ -f /boot/grub/grub.cfg ] && sed -i 's/default="1>0"/default="0"/' /boot/grub/grub.cfg >/dev/null 2>&1
 elif [ "$KERNEL" = "6.6" ] && [ "$ARCH" = "amd64" ]; then
 	# awk command from xanmod website
 	PSABI=$(awk 'BEGIN { while (!/flags/) if (getline < "/proc/cpuinfo" != 1) exit 1; if (/lm/&&/cmov/&&/cx8/&&/fpu/&&/fxsr/&&/mmx/&&/syscall/&&/sse2/) level = 1; if (level == 1 && /cx16/&&/lahf/&&/popcnt/&&/sse4_1/&&/sse4_2/&&/ssse3/) level = 2; if (level == 2 && /avx/&&/avx2/&&/bmi1/&&/bmi2/&&/f16c/&&/fma/&&/abm/&&/movbe/&&/xsave/) level = 3; if (level == 3 && /avx512f/&&/avx512bw/&&/avx512cd/&&/avx512dq/&&/avx512vl/) level = 4; if (level > 0) { print "x64v" level; exit level + 1 }; exit 1;}' | tr -d "\n")
